946f52d3223d363b0aa5a2b6e8d0d08b724e047f
EmileJ <72444841+EmileJ@users.noreply.github.com>
2020-10-13 10:31:48 +0200

    Deel 2 klaar
    maakt een nieuw object, en tracked daarin hoe vaak het aantal ogen gegooid is

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ gooi_btn.addEventListener("click",gooi);
 function gooi(){
     var worpen = [];
 
+    const worp = {
+        1:0,
+        2:0,
+        3:0,
+        4:0,
+        5:0,
+        6:0
+    }
+
     for(i = 0; i<8; i++){
         var getal = Math.floor(Math.random() * 6)+1;
+        worp[getal]++;
         worpen[i] = getal;
     }
     
-    console.log(worpen);
+    console.log(worp);
 }
